7f29c050f88553d6a2c2b5f67944d19ae5b6e8cb
SerggAidd <Aitov.S.V@gmail.com>
2021-05-05 20:20:05 +0300

    Start

---
+++ app/Http/Controllers/GroupController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GroupController extends Controller
+{
+   public function get(Type $var = null)
+   {
+        return 'Hello World';
+   }
+   public function create(Type $var = null)
+   {
+        return 'Hello World';
+   }
+   public function edit(Type $var = null)
+   {
+        return 'Hello World';
+   }
+   public function delete(Type $var = null)
+   {
+        return 'Hello World';
+   }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function get(Type $var = null)
+    {
+         return 'Hello World';
+    }
+    public function create(Type $var = null)
+    {
+         return 'Hello World';
+    }
+    public function edit(Type $var = null)
+    {
+         return 'Hello World';
+    }
+    public function delete(Type $var = null)
+    {
+         return 'Hello World';
+    }
+         
+}
